31a2f71a1ff1f54f87d63d44125b7d6a025ead97
Denis <denisrubel40@gmail.com>
2022-09-26 14:21:11 +0300

    CRUT operations for Category

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -27,7 +27,69 @@ namespace Rocky.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category category)
         {
-            _db.Categories.Add(category);
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                NotFound();
+            }
+
+            _db.Categories.Remove(obj);
             _db.SaveChanges();
 
             return RedirectToAction("Index");
--- Rocky/Models/Category.cs
+++ Rocky/Models/Category.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Rocky.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Microsoft.Build.Framework.Required]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Microsoft.Build.Framework.Required]
+        [Range(1,Int32.MaxValue, ErrorMessage = "Display Order for category must be greater than 0")]
         public int DisplayOrder { get; set; }
     }
 }
